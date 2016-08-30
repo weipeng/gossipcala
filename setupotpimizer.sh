1164a93ff90f9a3a2308171cf74c01289891259b
Yanyang Wang <yanyang.wang@ocado.com>
2016-08-30 14:10:59 +0100

    update scala oprimizer git link

--- setupotpimizer.sh
+++ setupotpimizer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone git://github.com/magarciaEPFL/scala.git GenRefactored99sZ
+git clone https://github.com/magarciaEPFL/scala.git GenRefactored99sZ
 cd GenRefactored99sZ
 git checkout -b GenRefactored99sZ origin/GenRefactored99sZ
 ant all.clean && ant
