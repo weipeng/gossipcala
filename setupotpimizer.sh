d944a25e71aecd1a7d5d30d538912a18d700eae0
Weipeng Huang <wphuang7@gmail.com>
2016-09-15 15:20:00 +0100

    remove the optimizer

--- setupotpimizer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/magarciaEPFL/scala.git GenRefactored99sZ
-cd GenRefactored99sZ
-git checkout -b GenRefactored99sZ origin/GenRefactored99sZ
-ant all.clean && ant
