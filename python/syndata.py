ce9790d1edbf481dcdfaa25eb3b951524b9fa873
Weipeng Huang <wphuang7@gmail.com>
2016-09-18 16:58:10 +0100

    fix the path

--- python/syndata.py
+++ python/syndata.py
@@ -2,16 +2,17 @@ from numpy.random import *
 from numpy import savetxt
 
 if __name__ == '__main__':
+    prefix = '../syntheticdata/data'
     for n in xrange(200, 1001, 200):
         normal_data = normal(100, 100, n)
-        savetxt('data/normal_100_%d.csv.gz' % n, normal_data)
+        savetxt('%s/normal_100_%d.csv.gz' % (prefix, n), normal_data)
 
         normal_data = normal(100, 1000, n)
-        savetxt('data/normal_1000_%d.csv.gz' % n, normal_data)
+        savetxt('%d/normal_1000_%d.csv.gz' % (prefix, n), normal_data)
 
         logit_data = logistic(100, 100, n)
-        savetxt('data/logit_100_%d.csv.gz' % n, logit_data)
+        savetxt('%d/logit_100_%d.csv.gz' % (prefix, n), logit_data)
 
         logit_data = logistic(100, 1000, n)
-        savetxt('data/logit_1000_%d.csv.gz' % n, logit_data)
+        savetxt('%d/logit_1000_%d.csv.gz' % (prefix, n), logit_data)
 
