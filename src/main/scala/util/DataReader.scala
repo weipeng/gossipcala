117144dba9509bf9ab7e330c15cbbd696d7a96fe
Weipeng Huang <wphuang7@gmail.com>
2016-09-18 16:54:00 +0100

    add data reader

---
+++ src/main/scala/util/DataReader.scala
@@ -0,0 +1,23 @@
+package util
+
+import com.typesafe.scalalogging.LazyLogging
+import breeze.linalg.DenseVector
+
+
+class DataReader extends LazyLogging {
+  lazy val pathPrefix = "syntheticdata"
+
+  def read(fileName: String): DenseVector[Double] = {
+    val projectPath = new java.io.File(".").getCanonicalPath
+    val targetFilePath = List(projectPath, pathPrefix, fileName).mkString("/")
+
+    logger.info(s"reading synthetic data from $targetFilePath")
+    val data = GzFileIterator(targetFilePath).toList.map{_.toDouble} 
+    data match {
+      case Nil =>
+        throw new RuntimeException(s"$targetFilePath is empty")
+      case x :: _ => x
+    }
+    DenseVector(data.toArray)
+  }
+}
