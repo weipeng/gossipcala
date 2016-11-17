0487234917c47417e5708e5023cd41fc196e0130
Weipeng Huang <kraken.huang@gmail.com>
2016-11-17 11:51:47 +0000

    modified plotting function

--- python/plotting/plot.py
+++ python/plotting/plot.py
@@ -22,30 +22,65 @@ def plot(log, feature):
     plt.show() 
     plt.close()
 
-def plot_sens_analysis(log, feature):
-    plt.figure(figsize=(3.2, 3.7))
+def plot_sens_analysis(log1, log2, feature):
+    fig = plt.figure(figsize=(3.9, 4.1))
+    ax = fig.add_subplot(111)
+    ax1 = ax.twinx()
     #plt.rcParams['text.usetex'] = True
 
-    df = pd.read_csv(log)
+    df = pd.read_csv(log1)
     df = df[['stoppingThreshold', 'gossipType', 'simCounter', feature]]
 
     df = df.rename(columns={'stoppingThreshold': 'stopping threshold',
                             'meanL1AbsoluteError': 'mean L1 absolute error',
                             'gossipType': 'gossip type'})
-    ax = sns.tsplot(time='stopping threshold', value='mean L1 absolute error',
+    sns.tsplot(time='stopping threshold', value='mean L1 absolute error',
                     unit='simCounter', condition='gossip type',
-                    interpolate=True,
-                    err_style= 'ci_band', #['ci_band', 'ci_bars', 'unit_traces'], #'unit_points'],
-                    data=df, ci=99, color='black')
+                    interpolate=True, ax=ax,  
+                    err_style= 'ci_bars', #['ci_band', 'ci_bars', 'unit_traces'], #'unit_points'],
+                    data=df, ci=99, color='black', marker='^')
+    
 
-    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0), labelsize=7, useOffset=False)
+    lgds = [r'$\mathbf{\mathcal{N}(0, 100)}$', r'$\mathbf{\mathcal{N}(0, 1000)}$']
+    lgds = ['low variance', 'high variance']
+
+
+    
+    df1 = pd.read_csv(log2)
+    df1 = df1[['stoppingThreshold', 'gossipType', 'simCounter', feature]]
+
+    df1 = df1.rename(columns={'stoppingThreshold': 'stopping threshold',
+                              'meanL1AbsoluteError': 'mean L1 absolute error',                            
+                              'gossipType': 'gossip type'})
+
+    sns.tsplot(time='stopping threshold', value='mean L1 absolute error',
+                unit='simCounter', condition='gossip type',
+                #interpolate=True, 
+                err_style= 'ci_band', #['ci_band', 'ci_bars', 'unit_traces'], #'unit_points'],
+                data=df1, ci=99, ax=ax1, color='r')
+    
+    #ax1.lines[-1].set_marker("o")
+    ax1.yaxis.label.set_visible(False)
+
+    handles, labels = ax.get_legend_handles_labels()
+    handles1, labels1 = ax1.get_legend_handles_labels()
+    ax.legend_.remove()
+    ax1.legend_.remove()
+    plt.legend(handles=handles+handles1, labels=lgds, loc=0, prop={'weight': 'bold'})
+    for tl in ax1.get_yticklabels():
+        tl.set_color('r')
+
+    ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0), labelsize=7, useOffset=False)
+    ax1.ticklabel_format(style='sci', axis='y', scilimits=(0,0), labelsize=7, useOffset=False)
     plt.xlabel(r'stopping threashold $\tau$')
-    plt.legend()
+    plt.title('Push Sum')
+    
     plt.tight_layout()
-    plt.savefig('%s.pdf' % log.rstrip('.csv'), format='pdf')
+    plt.savefig('%s.pdf' % log1.rstrip('.csv'), format='pdf')
+
     plt.close()
     
 if __name__ == '__main__':
     import sys
     
-    plot_sens_analysis(sys.argv[1], 'meanL1AbsoluteError')
+    plot_sens_analysis(sys.argv[1], sys.argv[2], 'meanL1AbsoluteError')
