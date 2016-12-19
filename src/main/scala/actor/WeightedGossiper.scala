2cd39a049e5d6e88cc34d1fbe78db85f5197c9f1
Weipeng Huang <kraken.huang@gmail.com>
2016-12-19 22:17:32 +0000

    a small fix to weighted gossiper

--- src/main/scala/actor/WeightedGossiper.scala
+++ src/main/scala/actor/WeightedGossiper.scala
@@ -19,42 +19,55 @@ class WeightedGossiper(override val name: String,
       val neighbors = nbs + (name -> self)
       context become work(neighbors,
         gossiper.bumpRound(),
-        wState.copy(diffuseMatrix = neighbors.values.map(_ -> 1.0 / neighbors.size).toMap))
+        wState.copy(
+          diffuseMatrix = neighbors.values.map(_ -> 1.0 / neighbors.size).toMap)
+        )
 
     case StartMessage(_) =>
       context become work(neighbors, gossip(gossiper, wState), wState)
 
-    case WeightedPushMessage(data, gossiper.roundCount) =>
+    case WeightedPushMessage(data, roundCount) =>
       log.debug(s"$name in ${gossiper.roundCount} receive $data from ${GossiperActorTrait.extractName(sender)}")
       val mailBoxState = wState.mailbox :+ data
       if (sender == self) {
-        val newState = gossiper.copy(data = mailBoxState.fold(DenseVector(0.0, 0.0))((x, y) => x + y)).compareData()
+        val newState = gossiper.copy(
+          data = mailBoxState.fold(DenseVector(0.0, 0.0))((x, y) => x + y)
+        ).compareData()
+
         if (newState.toStop()) {
-          context become work(neighbors, newState, wState.copy(mailbox = Vector.empty))
+          context become work(neighbors, newState, 
+                              wState.copy(mailbox = Vector.empty))
           self ! StopMessage
         } else {
-          context become work(neighbors, newState.bumpRound(), wState.copy(mailbox = Vector.empty))
+          context become work(neighbors, newState.bumpRound(), 
+                              wState.copy(mailbox = Vector.empty))
           self ! StartMessage(None)
         }
       } else {
-        context become work(neighbors, gossiper, wState.copy(mailbox = mailBoxState))
+        context become work(neighbors, gossiper, 
+                            wState.copy(mailbox = mailBoxState))
       }
 
     case StopMessage =>
       context become work(neighbors, gossiper.wrap(), wState)
   }
 
-  private def gossip(gossiper: SingleMeanGossiper, wState: WeightExtraState): SingleMeanGossiper = {
+  private def gossip(gossiper: SingleMeanGossiper, 
+                     wState: WeightExtraState): SingleMeanGossiper = {
     val diffuseMat = wState.diffuseMatrix
-    diffuseMat.foreach {
-      case (`self`, v) => sendSelfWithDelay(WeightedPushMessage(gossiper.data * v, gossiper.roundCount))
-      case (other, v) => other ! WeightedPushMessage(gossiper.data * v, gossiper.roundCount)
+    for ((x, v) <- diffuseMat) { 
+      if (x != self) {
+        log.debug(s"$name sends to $x")
+        x ! WeightedPushMessage(gossiper.data * v, gossiper.roundCount)
+      }
     }
+  
+    sendSelfWithDelay(WeightedPushMessage(gossiper.data * diffuseMat(self), gossiper.roundCount))
     gossiper.copy(messageCount = gossiper.messageCount + diffuseMat.size)
   }
 
   override val defaultExtraState = WeightExtraState(Map.empty, Vector.empty)
-  override val waitTime: FiniteDuration = 50 millis
+  override val waitTime: FiniteDuration = 100 millis
 }
 
 object WeightedGossiper {
