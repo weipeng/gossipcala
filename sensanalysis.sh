0c815048d7300990b4ba952e92528eacf8a671e4
Weipeng Huang <kraken.huang@gmail.com>
2016-12-23 23:04:21 +0800

    enhanced script

--- sensanalysis.sh
+++ sensanalysis.sh
@@ -1,20 +1,25 @@
 rm /tmp/gossipcala.log
 
-#for type in "pushsum" "pushpull"
 for num in 200 400 600 800 1000
 do 
-  for type in "weighted"
+  for ds in "normal_10" "normal_100" "normal_1000"
   do
-    echo $type
-    for i in {3..30}
+    for type in "weighted" #"pushsum" "pushpull"
     do
-    if (( $i % 3 != 0 ))
-    then
-      echo "We skip the threshold $i"
-      continue 
-    fi
-    echo "Testing threhold $i"
-    sbt run "-Dalgorithm.stopping-threshold=$i -Dsimulation.gossip-type=$type -Dsimulation.num-nodes=$num"
+      echo $type $ds $num
+      for i in {3..30}
+      do
+      if (( $i % 3 != 0 ))
+      then
+        echo "We skip the threshold $i"
+        continue 
+      fi
+      echo "Testing threhold $i"
+      sbt run "-Dalgorithm.stopping-threshold=$i \
+               -Dsimulation.gossip-type=$type \
+               -Dsimulation.num-nodes=$num \
+               -Dsimulation.data-source=$ds"
+      done
     done
   done
 done
