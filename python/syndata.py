69b1e7a347d0ef1914533064fae1643351dcb453
Weipeng Huang <kraken.huang@gmail.com>
2017-01-19 17:38:17 +0000

    updated synthetic data

--- python/syndata.py
+++ python/syndata.py
@@ -1,14 +1,19 @@
 from numpy.random import *
 from numpy import savetxt
 
+
 if __name__ == '__main__':
     prefix = '../syntheticdata/data'
-    for n in xrange(200, 1001, 200):
+    for n in xrange(5000, 5001, 200):
         #normal_data = normal(100, 100, n)
         #savetxt('%s/normal_100_%d.csv.gz' % (prefix, n), normal_data)
 
-        normal_data = normal(100, 1000, n)
-        savetxt('%s/normal_1000_%d.csv.gz' % (prefix, n), normal_data)
+        while True:
+            normal_data = normal(100, 1000, n)
+            print normal_data.mean(), normal_data.std()
+            if abs(normal_data.mean()/100-1) < 0.01 and abs(normal_data.std()/1000-1) < 0.01: 
+                savetxt('%s/normal_1000_%d.csv.gz' % (prefix, n), normal_data)
+                break
 
         #logit_data = logistic(100, 100, n)
         #savetxt('%s/logit_100_%d.csv.gz' % (prefix, n), logit_data)
