9788c9032f7a6ce6d28f0c090ed232024c4b80fa
Weipeng Huang <kraken.huang@gmail.com>
2017-02-06 15:18:20 +0000

    better code for parallelism

--- python/graphproperty.py
+++ python/graphproperty.py
@@ -4,74 +4,65 @@ import networkx as nx
 import numpy as np
 import simplejson as json
 from networkx.readwrite import json_graph
-import Queue
-import multiprocessing as mp
+from joblib import Parallel, delayed
 
 
-def handle(queue, i):
-    doc = ['Order', 'Index', 'Mean degree', 
+def get_graph_property(fn, i):
+    doc = ['Order', 'Index', 'Mean degree',
            'Variance of degree', 'Min degree', 'Max degree',
            'Radius', 'Diameter', 'Density', 'Connectivity',
+           'Mean eccentricity', 'Variance of eccentricity', 'Min eccentricity', 'Max eccentricity',
            'Mean clustering', 'Variance of clustering', 'Min clustering', 'Max clustering']
 
     with open('graphproperties_%d.csv' % i, 'ab') as fout:
-        fout.write('\t'.join(doc))
-        fout.write('\n')
+        fout.write('\t'.join(doc)+'\n')
+        if 'sw' in fn: return
 
-        while True:
-            try:
-                fn = queue.get(True, 0.01)
-            except Queue.Empty:
-                print i, 'stop here'
-                break                
+        print 'Processing', fn
+        with gzip.open('../graphs/'+fn) as f:
+            jdata = json.load(f)
 
-            if 'sw' in fn: continue
+        index = jdata['index']
+        del jdata['index']
+        del jdata['order']
+        mean_degree = jdata['mean_degree']
+        del jdata['mean_degree']
+        var_degree = jdata['var_degree']
+        del jdata['var_degree']
+        del jdata['meanSharedNeighbors']
 
-            print 'Processing', fn
-            with gzip.open('../graphs/'+fn) as f:
-                jdata = json.load(f)
+        G = json_graph.node_link_graph(jdata)
+        min_degree = np.min(G.degree().values())
+        max_degree = np.max(G.degree().values())
 
-            index = jdata['index']
-            del jdata['index']
-            del jdata['order']
-            mean_degree = jdata['mean_degree']
-            del jdata['mean_degree']
-            var_degree = jdata['var_degree']
-            del jdata['var_degree']
-            del jdata['meanSharedNeighbors']
+        ecc = nx.eccentricity(G)
+        min_ecc = np.min(ecc.values())
+        max_ecc = np.max(ecc.values())
+        mean_ecc = np.mean(ecc.values())
+        var_ecc = np.var(ecc.values())
+        radius = min_ecc
+        diameter = max_ecc
+        #center = nx.center(G)
+        #periphery = nx.periphery(G)
+        density = nx.density(G)
+        connectivity = nx.node_connectivity(G)
 
-            G = json_graph.node_link_graph(jdata)
-            min_degree = np.min(G.degree().values())
-            max_degree = np.max(G.degree().values())
-            
-            radius = nx.radius(G)
-            diameter = nx.diameter(G)
-            #eccentricity = nx.eccentricity(G)
-            #center = nx.center(G)
-            #periphery = nx.periphery(G)
-            density = nx.density(G)
-            connectivity = nx.node_connectivity(G)
+        clustering = nx.clustering(G)
+        min_clust = np.min(clustering.values())
+        max_clust = np.max(clustering.values())
+        mean_clust = np.mean(clustering.values())
+        var_clust = np.var(clustering.values())
 
-            clustering = nx.clustering(G)
-            min_clust = np.min(clustering.values())
-            max_clust = np.max(clustering.values())
-            mean_clust = np.mean(clustering.values())
-            var_clust = np.var(clustering.values())
-            
-            row = [G.order(), index, mean_degree, 
-                   var_degree, min_degree, max_degree,
-                   radius, diameter, density, connectivity,
-                   mean_clust, var_clust, min_clust, max_clust]
+        row = [G.order(), index, mean_degree,
+               var_degree, min_degree, max_degree,
+               radius, diameter, density, connectivity,
+               mean_ecc, var_ecc, min_ecc, max_ecc,
+               mean_clust, var_clust, min_clust, max_clust]
 
-            fout.write('\t'.join(map(str, row)) + '\n')
+        fout.write('\t'.join(map(str, row)) + '\n')
 
 if __name__ == '__main__':
-    queue = mp.Queue()
-    map(queue.put, os.listdir('../graphs'))
-    
-    procs = [mp.Process(target=handle, args=(queue, i)) for i in xrange(8)]
-    for proc in procs:
-        proc.start()
-    
-    for proc in procs:
-        proc.join()
+    n_jobs = 8
+    Parallel(n_jobs=n_jobs)(
+        delayed(get_graph_property)(fn, i%n_jobs) 
+        for i, fn in enumerate(os.listdir('../graphs')))
