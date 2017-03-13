d3a5ba2cf255782af1ae2e67b58f0245b855bfd7
Weipeng Huang <kraken.huang@gmail.com>
2017-03-13 12:48:50 +0000

    plot heatmaps

---
+++ python/plotting/plotheatmap.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import seaborn as sns; sns.set()
+import pandas as pd
+
+plt.rc('text', usetex=True)
+corrs = ['Corr type = Pearson', 'Corr type = Spearman']
+
+for gtype in ['PUSHSUM', 'WEIGHTED']:
+    fig, axes = plt.subplots(1, 2, sharey=True, figsize=(5., 5.)) 
+
+    df1 = pd.read_csv('covs/%s_pea.csv' % gtype, index_col=0)
+    df2 = pd.read_csv('covs/%s_spe.csv' % gtype, index_col=0)
+
+    cbar_ax = fig.add_axes([.894, .3, .03, .4])
+    sns.heatmap(df1, ax=axes[0], cbar=False, linewidth=1, vmin=-1, vmax=1)
+    sns.heatmap(df2, ax=axes[1], linewidth=1, 
+                cbar_ax=cbar_ax, vmin=-1, vmax=1)
+
+    for i, corr in enumerate(corrs):
+        axes[i].set_title(corr, fontweight='bold')   
+
+    plt.setp(axes[0].get_yticklabels(), rotation=0)
+
+    plt.tight_layout(rect=[0, 0, .9, 1])
+    plt.savefig('%s-corr.pdf' % gtype.lower())
+
+plt.close('all')
